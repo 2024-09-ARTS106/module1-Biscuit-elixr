d3e9a2115a3fd8a8b79063286f1409a9630dc7d4
Biscuit-elixr <vasquezben24@gmail.com>
2024-09-28 16:16:58 -0400

    Ch 6: Rotation & DeltaTime
    Added rotation and Time.deltaTime

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed; 
+    public float speed;
+    public float rotationSpeed;
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X"); //horizontal axis
+        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0); //rotating along the Mouse X axis 
+
         //Move forward
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(0, 0, speed);
+            transform.Translate(0, 0, speed * Time.deltaTime);
         }
         //Move backward
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(0, 0, -speed);
+            transform.Translate(0, 0, -speed * Time.deltaTime);
         }
         //Move left
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(-speed, 0, 0);
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
         //Move right
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(speed, 0, 0);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
         }
 
     }
