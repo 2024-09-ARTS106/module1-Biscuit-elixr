b82aea879f50588a553db4680776bc46fffa3523
Biscuit-elixr <vasquezben24@gmail.com>
2024-09-30 16:45:55 -0400

    Ch 6: Implemented New Input Mapping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
 
-    void Start()
+
+    private Vector2 movementValue;
+    private float lookValue;
+
+    private void Awake()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-    
+
+    public void OnMove(InputValue value)
+    {
+        movementValue = value.Get<Vector2>() * speed;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookValue = value.Get<Vector2>().x * rotationSpeed;
+    }
+
+    /*void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }*/
+
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X"); //horizontal axis
+
+
+        transform.Translate(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
+
+        transform.Rotate(0, lookValue * Time.deltaTime, 0);
+
+        /*float mouseX = Input.GetAxis("Mouse X"); //horizontal axis
         transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0); //rotating along the Mouse X axis 
 
         //Move forward
@@ -37,7 +64,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
+        }*/
 
     }
 }
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerShooting : MonoBehaviour
 {
     public GameObject prefab;
     public GameObject shootPoint;
 
-    void Update()
+    /*public void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             Instantiate(prefab, shootPoint.transform.position, shootPoint.transform.rotation);
-            /*GameObject clone = Instantiate(prefab);
-            clone.transform.position = shootPoint.transform.position;
-            clone.transform.rotation = shootPoint.transform.rotation;*/
+            
         }
+    }*/
+
+    public void OnFire(InputValue value)
+    {
+        if(value.isPressed)
+        {
+            GameObject clone = Instantiate(prefab);
+
+            clone.transform.position = shootPoint.transform.position;
+            clone.transform.rotation = shootPoint.transform.rotation;
+        }
+       
     }
 }
