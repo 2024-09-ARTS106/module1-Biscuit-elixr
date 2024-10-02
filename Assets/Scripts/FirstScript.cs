82dc149d1df898e5d06b0dc7b268f4b23646eaf2
Biscuit-elixr <vasquezben24@gmail.com>
2024-10-02 11:04:10 -0400

    Added a loop on idle animations

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
+    [SerializeField] private float speed;
     //public float speed;
+    private int frameNum;
 
     // Start is called before the first frame update
     void Start()
     {
+        frameNum = 1; 
         print("test Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(speed);
+        //print(speed);
+        //print("Frame #{0}: Update at Time #{1}", frameNum, Time.time);
+        print("Frame " + frameNum + " Update at Time: " + Time.time);
+        frameNum = frameNum++;
     }
 
     void OnDestroy()
